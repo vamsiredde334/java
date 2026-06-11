58121f0643d5067df3bcadfe8f9f976b5a19912b
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-11 22:54:03 +0530

    Add files via upload

---
+++ na.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class na {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = 2; i <= n; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
