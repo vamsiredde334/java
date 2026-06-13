fbaa728db20e9d9c4e7433d28e58dd8b85827e1a
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-13 21:34:43 +0530

    Add files via upload

--- c.java
+++ c.java
@@ -3,13 +3,20 @@ public class c {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter your a:");
-        int a = sc.nextInt();
+        double a = sc.nextDouble();
+        System.out.println("enter your operation +,-,*,/,^:");
+        char s=  sc.next().charAt(0);
         System.out.println("enter your b:");
-        int b = sc.nextInt();
-        System.out.println("enter your c:");
-        int c = sc.nextInt();
-        int percentage = ((a+b+c)/3);
-        System.out.println(percentage);
+        Double b = sc.nextDouble();
+        double result=0;
+        switch (s) {
+            case '+' -> result = a+b;
+            case '-' -> result = a-b;
+            case '*' -> result = a*b;
+            case '/' -> result = a/b;
+            case '^' -> result = Math.pow(a, b);
+        }
+        System.out.println("Result: " + result);
 
     }
 }
