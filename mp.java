27a5706331c9c6c67840369a61da6ea06e680ddb
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-13 22:28:37 +0530

    Add files via upload

---
+++ mp.java
@@ -0,0 +1,10 @@
+    public class mp {
+        public static void main(String[] args) {
+            name(8,9);
+            name(7,0);
+        }
+        static void name(int a,int b){
+            int c=a+b;
+            System.out.println(c);
+
+        }    }
