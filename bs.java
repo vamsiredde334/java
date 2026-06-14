22bb4d33cb8c5f199d3abd8bd728e1e578b0a0dc
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-14 23:37:28 +0530

    Add files via upload

---
+++ bs.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class bs{
+    static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double balance=0;
+        double withdraw;
+        double deposit;
+        System.out.println("*************");
+    System.out.println("WELCOME");
+        System.out.println("*************");
+        int a= sc.nextInt();
+        System.out.println("Enter the number 1-balance 2-withdraw 3-deposit 4-exit" + a);
+    }
+    
+}
