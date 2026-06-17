ce1ff0a245a62ee8113979cf28fe2dac91474dd4
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-17 22:32:19 +0530

    Add files via upload

---
+++ arrc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class arrc {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] marks = new int[4];
+
+        System.out.println("Enter 4 marks:");
+
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        System.out.println("Marks are:");
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
