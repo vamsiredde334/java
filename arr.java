3433573b85e3e1167912addc7d6fe3c65677ade2
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-17 22:14:42 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,9 @@
+public class arr {
+    public static void main(String[] args) {
+        String [] name = { "vamsi" , "krishna" , "reddy"};
+        System.out.println(name[0]);
+        for(int i=0; i < name.length; i++ ){
+        System.out.println(name[i]);
+        }
+    }
+}
