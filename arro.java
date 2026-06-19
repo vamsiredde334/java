4913b6d60f17e40920dd0f98a4f615ed0ed8a11c
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-19 23:53:48 +0530

    Add files via upload

---
+++ arro.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class arro{
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("enter the elements " );
+    int n= sc.nextInt();
+    int[] ani= new int[n];
+    System.out.println("enter the elements  ");
+
+    for(int i=0; i<ani.length; i++){
+        ani[i]=sc.nextInt();
+    
+    }
+    int count=0;
+    for (int i = 0; i < ani.length; i++) {
+            if (ani[i] % 2 != 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Even Count = " + count);
+    }
+}
