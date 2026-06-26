d849deeba9726525e6938ad3483e7c38ffacb07f
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-26 21:15:13 +0530

    Add files via upload

---
+++ reversearr.java
@@ -0,0 +1,10 @@
+public class reversearr {
+    public static void main(String[] args) {
+        int[] v={1,3,2,4,6,5,7,9};
+        for(int i=v.length-1; i>=0; i--){
+            System.out.println(v[i]);
+
+        }
+    }
+    
+}
