2d2ee980a9c23635da14d75229073ce91cd013f0
vamsiredde334 <vamsikrishnareddy334@gmail.com>
2026-06-28 22:03:23 +0530

    Add files via upload

---
+++ tdmax.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class tdmax {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int r = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int c = sc.nextInt();
+
+        int[][] a = new int[r][c];
+
+        System.out.println("Enter the elements:");
+
+        // Input
+        System.out.println("elements of row");
+        for(int i = 0; i < r; i++){
+            System.out.println("elements of column");
+            for(int j = 0; j < c; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }int max=a[0][0];
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(a[i][j]>max){
+                    max=a[i][j];
+                }
+            }
+    }System.out.println("biggest of whole matrix is "+max);
+}
+}
